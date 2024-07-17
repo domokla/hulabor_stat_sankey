b48279c6a7532eb9b596d5695d957af6060d310e
domokla <laszlo.domok@outlook.com>
2024-07-17 18:42:12 +0200

    update colors

--- sankey_test.py
+++ sankey_test.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.io as pio
@@ -6,6 +5,7 @@ import os
 
 # Change directory if necessary
 os.chdir("/path/your/folder/hulabor_stat_sankey")
+
 
 # Excel file reading
 excel_path = 'data/munka.xlsx'
@@ -16,6 +16,7 @@ labels = df['status_en'].tolist()
 source = df['source_id'].tolist()
 target = df['target_id'].tolist()
 value = df['value'].tolist()
+node_colors = df['hexa'].tolist()
 
 # Create Sankey diagram
 fig = go.Figure(data=[go.Sankey(
@@ -24,7 +25,7 @@ fig = go.Figure(data=[go.Sankey(
         thickness=20,
         line=dict(color="black", width=0.5),
         label=labels,
-        color="blue"  # Node color
+        color=node_colors  # Node colors
     ),
     link=dict(
         source=source,
@@ -47,24 +48,28 @@ fig.update_layout(
     margin=dict(l=50, r=50, t=50, b=50),  # Margins around the plot
     width=1200,  # Diagram width in pixels
     height=800,  # Diagram height in pixels
+    
 )
+
 
 # Show the diagram in the default browser
 pio.renderers.default = 'browser'
 fig.show()
 
-html_file_path = os.path.join('output', 'sankey_diagram.html')
-
 # Save the diagram as an HTML file
+html_file_path = os.path.join('output', 'sankey_diagram.html')
 fig.write_html(html_file_path)
 
 # Generate image file path
 image_file_path = os.path.join('output', 'sankey_diagram.png')
 
 # Save the diagram as an image file
-fig.write_image(image_file_path, format="png", engine="kaleido", scale=5)  # Scale set to 3 for higher resolution
+fig.write_image(image_file_path, format="png", engine="kaleido", scale=5)  # Scale set to 5 for higher resolution
 
 print(f"Sankey diagram saved as {image_file_path}")
+
+
+
 
 
 
